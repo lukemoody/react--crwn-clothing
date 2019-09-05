9d0d48b762f80820725f99b1ad126d0f58c1155c
Luke Moody <luke@bgn.agency>
2019-09-05 14:21:24 +0100

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 // const HomePage = props => {
 //   console.log(props);
@@ -39,6 +40,7 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -4,7 +4,7 @@ import "./collection-item.style.scss";
 /**
  * 1. we don't need state so make a functional component
  * 2. Get directory items in the same way as before in const. This will be pulled down from SHOP_DATA shop.component/peview-collection
- * NOTE: The way the data has been passed down, there isn't a need to use this.props... like in MaltMap side project!
+ * NOTE: The way the data has been passed down, there isn't a need to use this.props... like in MaltMap side
  */
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
