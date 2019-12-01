30e7737a315dac949ed231ca0ffb174494fb39b0
Luke Moody <luke@bgn.agency>
2019-12-01 00:20:57 +0000

    Auth update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInUp from './pages/signinup/signinup.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.css';
 
 class App extends React.Component {
@@ -20,10 +20,31 @@ class App extends React.Component {
 
   componentDidMount() {
     // Save current signed in user
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // this.setState({ currentUser: user });
+      // createUserProfileDocument(user); // setState not working currently
 
-      console.log(user);
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+
+      // console.log(user);
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,37 @@ const config = {
   appId: '1:168719253941:web:490e1e998f2f683c7b19fc'
 };
 
+// Function for getting uid from google sign in to store in our DB
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // If userAuth desn't exist, return nothing
+  if (!userAuth) return;
+
+  // Get path to user reference
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  // Get user reference
+  const snapShot = await userRef.get();
+
+  // console.log(snapShot);
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
